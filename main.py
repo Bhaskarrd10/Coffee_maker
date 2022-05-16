437be42985e94848222f74d5eb653a07d4e82f80
bhaskarreddy2 <>
2022-05-16 15:21:13 +0530

    added more objects

--- main.py
+++ main.py
@@ -21,5 +21,6 @@ while is_on:
     else:
         drink = menu.find_drink(choice)
         if coffee_maker.is_resource_sufficient(drink):
-            print(money_machine.make_payment(drink.cost))
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
 
